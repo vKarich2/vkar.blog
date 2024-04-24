fa1ab27cd7eec45e71e57f991849c5e3a588335c
vKarich2 <vladyslav.karapetov@nure.ua>
2024-04-24 23:18:26 +0200

    feat: added author to post-page

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -59,8 +59,18 @@ export default async function Post({ params }: { params: {postId: string}}) {
 				</div>
 				<article className='max-w-[900px] prose-p:text-[#696969] prose-p:dark:text-[#9c9c9c] font-Quicksand font-normal prose prose-headings:text-[#1c1c1c] dark:prose-headings:text-[#f0f0f0] prose-headings:font-Oswald prose-headings:font-medium prose-headings:text-2xl prose-headings:opacity-100 prose-img:mx-auto prose-img:rounded-2xl prose-img:w-[1320px] prose-li:text-[#696969] prose-li:dark:text-[#9c9c9c] prose-strong:dark:text-[#f0f0f0]'>
 					<section className='text-[#1c1c1c] my-10 text-lg leading-relaxed' dangerouslySetInnerHTML={{ __html: contentHtml }} />
-					<Link className='text-[#1c1c1c] dark:text-[#f0f0f0] font-Oswald font-semibold text-lg opacity-90 hover:opacity-100 transition-opacity duration-300 dark:text-[#f0f0f0] no-underline pb-4' href='/'> &#128072; Back to home</Link>
-				</article>
+					<div className=' w-full h-[0.5px] bg-[#696969] dark:bg-[#9c9c9c] rounded-full mb-10'></div>	
+				</article>	
+			</div>
+			<div className='flex flex-col max-w-[900px] ml-[210px]'>
+				<div className='flex'>
+					<Image className='max-w-[60px] h-[60px] rounded-full' src={author.image} alt='Author' width={60} height={60} sizes='60x60' priority={false} />
+					<div className='flex flex-col justify-center ml-5 text-[#696969] dark:text-[#9c9c9c]'>
+						<p className='font-Oswald font-semibold text-md text-[#1c1c1c] dark:text-[#f0f0f0]'>{author.name}</p>
+						<p className='font-Quicksand font-normal text-sm'>{author.job}</p>
+					</div>
+				</div>
+				<Link className='text-[#1c1c1c] dark:text-[#f0f0f0] font-Oswald font-semibold text-lg opacity-90 hover:opacity-100 transition-opacity duration-300 dark:text-[#f0f0f0] no-underline my-10' href='/'> &#128072; Back to home</Link>
 			</div>
 		</main>
 	);
